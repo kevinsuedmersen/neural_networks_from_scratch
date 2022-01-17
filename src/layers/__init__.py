7e79e46df2c5c80919c9257744f995ec7539e39c
kevinsuedmersen <kevinsuedmersen@gmail.com>
2022-01-17 21:03:52 +0100

    added docstrings

--- src/layers/__init__.py
+++ src/layers/__init__.py
@@ -7,20 +7,25 @@ logger = logging.getLogger(__name__)
 class Layer(ABC):
     @abstractmethod
     def init_parameters(self, *args, **kwargs):
+        """Initializes weights and biases of the current layer"""
         pass
 
     @abstractmethod
     def forward_propagate(self, *args, **kwargs):
+        """Computes the activations of the current layer"""
         pass
 
     @abstractmethod
     def backward_propagate(self, *args, **kwargs):
+        """Computes the error of the current layer"""
         pass
 
     @abstractmethod
     def compute_weight_gradients(self, *args, **kwargs):
+        """Computes the weight gradients of the current layer"""
         pass
 
     @abstractmethod
     def compute_bias_gradients(self, *args, **kwargs):
+        """Computes the bias gradients of the current layer"""
         pass
