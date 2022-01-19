e3c1fd8fc0dc946db82127caf7ae2e1e1357c901
ksu <kevin.suedmersen@camao.one>
2022-01-19 20:01:50 +0100

    added structure for comparing gradients

--- tests/test_backward_propagation.py
+++ tests/test_backward_propagation.py
@@ -24,25 +24,50 @@ class TestBackwardPropagation(TestConfig):
     def fixed_model(self, config_parser):
         return get_model(config_parser.model_name)
 
-    def test_weight_gradients(self, ml_job, config_parser, fixed_model):
-        """Tests that the backward propagation algorithm computes the correct weight gradients"""
-        # Extract all weight gradients and store them in a flattened numpy array
-        # Flattened, because each layer's weight matrices have different dimensions
-        weight_gradients = []
+    @staticmethod
+    def _compute_gradients_backprop(ml_job, gradient_type="weight_gradients"):
+        """Computes the weight or bias gradients using backpropagation"""
+        # Extract all gradients and store them in a flattened numpy array
+        # Flattened, because each layer's weight matrices / bias vectors have different dimensions
+        gradients = []
         for layer in ml_job.model.layers[1:]:
-            flat_weight_gradients = layer.weight_gradients.ravel().reshape(-1, 1)
-            weight_gradients.append(flat_weight_gradients)
+            flat_gradients = getattr(layer, gradient_type).ravel().reshape(-1, 1)
+            gradients.append(flat_gradients)
+
         # shape=(n_layers, n_neurons, n_neurons_prev)
-        weight_gradients_backprop = np.concatenate(weight_gradients, axis=0)
-        print()
+        gradients_backprop = np.concatenate(gradients, axis=0)
 
-        # Forward propagate ``model`` and cache the loss value
+        return gradients_backprop
 
-        # Compute the weight gradients using the brute force method and store them in a numpy array
-        # For each training sample, for each layer, for each weight:
-        # - change the weight slightly and re-compute the loss value
-        # - calculate the partial derivative as (L(slightly_changed_weight) - L(original_weights)) / slight_change
-
-    def test_bias_gradients(self, ml_job):
-        """Tests that the backward propagation algorithm computes the correct bias gradients"""
+    def _compute_gradients_brute_force(self, ml_job, gradient_type="weight_gradients"):
+        """Computes the weight or bias gradients using a brute force method. Each derivative is
+        calculated by computing the losses after slightly changing one parameter each time while
+        keeping all other parameters constant, then subtracting the loss value computed with the
+        constant parameters and then dividing that difference by the slight change, i.e.:
+        (L(slightly_changed_parameters, all_other_parameters) - L(original_parameters)) / slight_change
+        """
         pass
+
+    @pytest.fixture
+    def mean_absolute_percentage_error(self):
+        pass
+
+    def test_weight_gradients(self, ml_job, config_parser, fixed_model, mean_absolute_percentage_error):
+        """Tests that the backward propagation algorithm computes the correct weight gradients"""
+        weight_gradients_backprop = self._compute_gradients_backprop(ml_job, "weight_gradients")
+        weight_gradients_brute_force = self._compute_gradients_brute_force(ml_job, "weight_gradients")
+        np.testing.assert_allclose(weight_gradients_backprop, weight_gradients_brute_force)
+        mean_absolute_percentage_error = mean_absolute_percentage_error.result(
+            weight_gradients_backprop,
+            weight_gradients_brute_force
+        )
+
+    def test_bias_gradients(self, ml_job, mean_absolute_percentage_error):
+        """Tests that the backward propagation algorithm computes the correct bias gradients"""
+        bias_gradients_backprop = self._compute_gradients_backprop(ml_job, "bias_gradients")
+        bias_gradients_brute_force = self._compute_gradients_brute_force(ml_job, "bias_gradients")
+        np.testing.assert_allclose(bias_gradients_backprop, bias_gradients_brute_force)
+        mean_absolute_percentage_error = mean_absolute_percentage_error.result(
+            bias_gradients_backprop,
+            bias_gradients_brute_force
+        )
