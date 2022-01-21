08f6ffb28e3a5cd004052335d84dd8b383730e62
ksu <kevin.suedmersen@camao.one>
2022-01-21 21:02:49 +0100

    added small model for tests

--- src/model_architectures.py
+++ src/model_architectures.py
@@ -2,12 +2,13 @@ import logging
 
 import numpy as np
 
-from src.layers.dense import DenseLayer
-from src.layers.input import InputLayer
-from src.losses.categorical_crossentropy import CategoricalCrossEntropyLoss
-from src.metrics.metrics import Accuracy
-from src.models.sequential import Model, SequentialModel
-from src.optimizers.stochastic_gradient_descent import StochasticGradientDescentOptimizer
+from src.lib.layers.dense import DenseLayer
+from src.lib.layers.input import InputLayer
+from src.lib.losses.categorical_crossentropy import CategoricalCrossEntropyLoss
+from src.lib.metrics.metrics import Accuracy
+from src.lib.models import Model
+from src.lib.models.sequential import SequentialModel
+from src.lib.optimizers.stochastic_gradient_descent import StochasticGradientDescentOptimizer
 
 logger = logging.getLogger(__name__)
 
@@ -25,15 +26,32 @@ def get_simple_mlp_model(
         optimizer=StochasticGradientDescentOptimizer()
     )
     mlp.add_layer(InputLayer(input_shape=(None, img_height, img_width, n_color_channels), layer_idx=0))
-    mlp.add_layer(DenseLayer(512, "relu", layer_idx=1))
-    mlp.add_layer(DenseLayer(256, "relu", layer_idx=2))
-    mlp.add_layer(DenseLayer(128, "relu", layer_idx=3))
-    mlp.add_layer(DenseLayer(64, "relu", layer_idx=4))
-    mlp.add_layer(DenseLayer(32, "relu", layer_idx=5))
-    mlp.add_layer(DenseLayer(16, "relu", layer_idx=6))
+    mlp.add_layer(DenseLayer(512, "tanh", layer_idx=1))
+    mlp.add_layer(DenseLayer(256, "tanh", layer_idx=2))
+    mlp.add_layer(DenseLayer(128, "tanh", layer_idx=3))
+    mlp.add_layer(DenseLayer(64, "tanh", layer_idx=4))
+    mlp.add_layer(DenseLayer(32, "tanh", layer_idx=5))
+    mlp.add_layer(DenseLayer(16, "tanh", layer_idx=6))
     mlp.add_layer(DenseLayer(2, "softmax", layer_idx=7))
 
-    # TODO: Use tanh instead of relu activation function
+    return mlp
+
+
+def get_tiny_mlp_model(
+        img_height: int,
+        img_width: int,
+        n_color_channels: int
+) -> SequentialModel:
+    """Creates a tiny Multi Layer Perceptron network for gradient computation tests"""
+    mlp = SequentialModel(
+        loss=CategoricalCrossEntropyLoss("softmax", "multi_class_classification"),
+        metrics_train=[Accuracy("acc_train")],
+        metrics_val=[Accuracy("acc_val")],
+        optimizer=StochasticGradientDescentOptimizer()
+    )
+    mlp.add_layer(InputLayer(input_shape=(None, img_height, img_width, n_color_channels), layer_idx=0))
+    mlp.add_layer(DenseLayer(32, "tanh", layer_idx=4))
+    mlp.add_layer(DenseLayer(2, "softmax", layer_idx=7))
 
     return mlp
 
