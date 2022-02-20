bdbd9c2ad440113dd155db1f247372a3f45ec2d0
ksu <kevin.suedmersen@camao.one>
2022-02-20 15:18:10 +0100

    Simplified code

--- src/lib/metrics/accuracy.py
+++ src/lib/metrics/accuracy.py
@@ -1,47 +1,16 @@
 import logging
 
-import numpy.typing as npt
-
 from src.lib.metrics import Metric
 
 logger = logging.getLogger(__name__)
 
 
 class Accuracy(Metric):
-    def __init__(self, name: str, threshold: float = 0.5, average_type: str = "micro"):
-        super().__init__(name)
-        self.threshold = threshold
-        self.average_type = average_type
-
-        # Internal counters / state trackers
-        self.all_correct_preds = 0
-        self.all_preds = 0
-
-    def _validate_input(self):
-        supported_average_types = ["micro"]
-        if self.average_type not in supported_average_types:
-            raise ValueError(
-                f"Unsupported average type: {self.average_type}. "
-                f"Supported average types are: {supported_average_types}"
-            )
-
-    def update_state(self, ytrue: npt.NDArray, ypred: npt.NDArray):
-        """Count and updates number of correct predictions and number of all predictions"""
-        n_true_positives, n_false_positives, n_true_negatives, n_false_negatives = self._count(
-            ytrue,
-            ypred,
-            self.threshold
-        )
-        self.all_correct_preds += (n_true_positives + n_true_negatives)
-        self.all_preds += (n_true_positives + n_false_positives + n_true_negatives + n_false_negatives)
-
     def result(self):
         """Returns the percentage of correct predictions from all predictions"""
-        accuracy_value = self.all_correct_preds / self.all_preds
+        accuracy_value = (
+                (self.all_true_positives + self.all_true_negatives) /
+                (self.all_true_positives + self.all_false_positives + self.all_true_negatives + self.all_false_negatives)
+        )
 
         return accuracy_value
-
-    def reset_state(self):
-        """Resets internal counters"""
-        self.all_correct_preds = 0
-        self.all_preds = 0
--- src/lib/metrics/precision.py
+++ src/lib/metrics/precision.py
@@ -1,7 +1,6 @@
 import logging
 
 import numpy as np
-import numpy.typing as npt
 
 from src.lib.metrics import Metric
 
@@ -9,32 +8,9 @@ logger = logging.getLogger(__name__)
 
 
 class Precision(Metric):
-    def __init__(self, name: str, threshold: float = 0.5, average_type: str = "micro"):
-        super().__init__(name)
-        self.threshold = threshold
-        self.average_type = average_type
-
-        self.all_true_positives = 0
-        self.all_false_positives = 0
-
-    def update_state(self, ytrue: npt.NDArray, ypred: npt.NDArray):
-        """Update number of true and false positives"""
-        n_true_positives, n_false_positives, _, _ = self._count(
-            ytrue,
-            ypred,
-            self.threshold
-        )
-        self.all_true_positives += n_true_positives
-        self.all_false_positives += n_false_positives
-
     def result(self):
         """Compute precision and return NaN if there are no predictions of the positive class"""
         if (self.all_true_positives + self.all_false_positives) == 0:
             return np.nan
         else:
             return self.all_true_positives / (self.all_true_positives + self.all_false_positives)
-
-    def reset_state(self):
-        """Resets the counters of all true and all false positives"""
-        self.all_true_positives = 0
-        self.all_false_positives = 0
