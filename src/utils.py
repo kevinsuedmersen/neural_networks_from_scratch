4812125a5d2f88cc60eadd14e243107a33d1b0b4
ksu <kevin.suedmersen@camao.one>
2022-02-25 22:20:03 +0100

    also logging line of code

--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,7 @@ def set_root_logger():
     """
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
-    log_formatter = logging.Formatter('%(asctime)s; %(name)s; %(levelname)s; %(message)s')
+    log_formatter = logging.Formatter('%(asctime)s; %(name)s.py:%(lineno)s; %(levelname)s; %(message)s')
     console_handler.setFormatter(log_formatter)
     root_logger = logging.getLogger()
     root_logger.setLevel(logging.INFO)
