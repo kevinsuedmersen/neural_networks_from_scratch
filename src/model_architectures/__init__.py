6c8b5cdf6765b50a951ab19b9f5dc48cc56c0092
ksu <kevin.suedmersen@camao.one>
2022-02-27 16:48:01 +0100

    created extra package for models

---
+++ src/model_architectures/__init__.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from src.lib.models import Model
+from src.model_architectures.own_models import get_simple_mlp_model
+
+
+def get_model(
+        model_name: str,
+        img_height: int = None,
+        img_width: int = None,
+        n_color_channels: int = None,
+        random_state: int = None,
+        n_classes: int = None,
+        learning_rate: int = None
+) -> Model:
+    if random_state is not None:
+        np.random.seed(random_state)
+
+    if model_name == "simple_mlp":
+        return get_simple_mlp_model(img_height, img_width, n_color_channels, n_classes, learning_rate)
+    else:
+        ValueError(f"Unknown or non-implemented model_name provided: {model_name}")
--- src/model_architectures/own_models.py
+++ src/model_architectures/own_models.py
@@ -1,7 +1,5 @@
 import logging
 
-import numpy as np
-
 from src.lib.layers.dense import DenseLayer
 from src.lib.layers.input import InputLayer
 from src.lib.losses.categorical_crossentropy import CategoricalCrossentropyLoss
@@ -9,7 +7,6 @@ from src.lib.metrics.cost.categorical_crossentropy import CategoricalCrossentrop
 from src.lib.metrics.score.accuracy import Accuracy
 from src.lib.metrics.score.precision import Precision
 from src.lib.metrics.score.recall import Recall
-from src.lib.models import Model
 from src.lib.models.sequential import SequentialModel
 from src.lib.optimizers.stochastic_gradient_descent import StochasticGradientDescentOptimizer
 
@@ -60,8 +57,8 @@ def get_tiny_mlp_model(
     """Creates a tiny Multi Layer Perceptron network for gradient computation tests"""
     mlp = SequentialModel(
         loss=CategoricalCrossentropyLoss("softmax", "multi_class_classification"),
-        metrics_train=[Accuracy("acc_train")],
-        metrics_val=[Accuracy("acc_val")],
+        metrics_train=[Accuracy("acc_train", None)],
+        metrics_val=[Accuracy("acc_val", None)],
         optimizer=StochasticGradientDescentOptimizer()
     )
     mlp.add_layer(InputLayer(input_shape=(None, img_height, img_width, n_color_channels)))
@@ -70,20 +67,3 @@ def get_tiny_mlp_model(
 
     return mlp
 
-
-def get_model(
-        model_name: str,
-        img_height: int = None,
-        img_width: int = None,
-        n_color_channels: int = None,
-        random_state: int = None,
-        n_classes: int = None,
-        learning_rate: int = None
-) -> Model:
-    if random_state is not None:
-        np.random.seed(random_state)
-
-    if model_name == "simple_mlp":
-        return get_simple_mlp_model(img_height, img_width, n_color_channels, n_classes, learning_rate)
-    else:
-        ValueError(f"Unknown or non-implemented model_name provided: {model_name}")
